f78614dbadfd8b2478b9489502d8eebefce5768f
Jose Felipe Blum de Araujo <j.felipe.araujo@outlook.com>
2021-03-15 19:32:33 -0300

    Criando repositorio de especificações

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute(): void {
-    // TO-DO
+  constructor(private specificationRepository: ISpecificationsRepository) {
+    // CreateSpecificationService
+  }
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationRepository.findByName(
+      name
+    );
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists");
+    }
+
+    this.specificationRepository.create({ name, description });
   }
 }
 
