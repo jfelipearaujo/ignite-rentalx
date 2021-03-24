9ac10c32924d275a80442f1ee79a5a3a1cb8f9f6
Jose Felipe Blum de Araujo <j.felipe.araujo@outlook.com>
2021-03-24 19:55:40 -0300

    Criptografando a senha do usuario

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -18,9 +19,19 @@ class CreateUserUseCase {
     email,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userEmailAlreadyExists = await this.usersRepository.findByEmail(
+      email
+    );
+
+    if (userEmailAlreadyExists) {
+      throw new Error("User e-mail already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      password,
+      password: passwordHash,
       email,
       driver_license,
     });
