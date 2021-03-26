0fc9b4c551cb1e1c06f22946223d48f3434310e7
Jose Felipe Blum de Araujo <j.felipe.araujo@outlook.com>
2021-03-25 21:19:59 -0300

    Deletando arquivos do avatar

---
+++ src/utils/file.ts
@@ -0,0 +1,11 @@
+import fs from "fs";
+
+export const deleteFile = async (filename: string) => {
+  try {
+    await fs.promises.stat(filename);
+
+    await fs.promises.unlink(filename);
+  } catch {
+    // Ignored if the file does not exists or if the file cannot be deleted
+  }
+};
